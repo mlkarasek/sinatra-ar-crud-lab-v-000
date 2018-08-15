4196dfb479ef0514a13eb4efb18cca61cc0bef1e
Monica  Karasek <mlkarasek@gmail.com>
2018-08-15 01:02:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,41 +7,66 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
+
+#CREATE
+
+  get '/' do
+
+  end
+
   get '/posts/new' do
     erb :new
   end
 
   post '/posts' do
-    @posts = Post.create(params)
+    @post = Post.create(params)
+
     redirect to '/posts'
   end
 
+#READ
+
   get '/posts' do
     @posts = Post.all
+
     erb :index
+
   end
 
   get '/posts/:id' do
     @post = Post.find_by_id(params[:id])
+
     erb :show
+
   end
 
-  get '/posts/:id/edit' do
-    @post = Post.find_by_id(params[:id])
-    erb :edit
-  end
+#UPDATE
+
+ get '/posts/:id/edit' do
+   @post = Post.find_by_id(params[:id])
+
+   erb :edit
+
+ end
 
   patch '/posts/:id' do
-    @post = Post.find_by_id(params[:id])
-    @post.name = params[:name]
-    @post.content = params[:content]
-    @post.save
-    erb :show
-  end
+   @post = Post.find_by_id(params[:id])
+   @post.name = params[:name]
+   @post.content = params[:content]
+   @post.save
 
-  delete '/posts/:id/delete' do
-    @post = Post.find_by_id(params[:id])
-    @post.delete
-    erb :delete 
-  end
+   erb :show
+
+ end
+
+#DELETE
+
+ delete '/posts/:id/delete' do
+    post = Post.find(params[:id])
+    post.delete
+    
+    erb :delete
+ end
+
 end
